19f61129587a1b129a64562b92fff0c936c8d130
robmars123 <robmars2008@gmail.com>
2017-02-23 03:41:38 -0600

    2/23/17

--- Project_Managment/Project_Managment/Models/Department.cs
+++ Project_Managment/Project_Managment/Models/Department.cs
@@ -5,18 +5,26 @@ namespace Project_Managment.Models
     using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
     using System.Data.Entity.Spatial;
+    using System.Web.Mvc;
 
     public partial class Department
     {
-        [Key]
+
+
+        public Department() { }
+        [Key, ForeignKey("Employees")]
+        [Display(Name = "User Role")]
         public int DepartmentID { get; set; }
 
         [StringLength(50)]
         public string DepartmentName { get; set; }
 
-       // public int EmployeeID { get; set; }
+        // public int EmployeeID { get; set; }
 
-       // public virtual Employee Employees { get; set; }
+     
+        public IEnumerable<SelectListItem> Departments { get; set; }
+
+        public virtual Employee Employees { get; set; }
 
     }
 }
