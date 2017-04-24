86aa96b2739af92b6c40cfea4258378325443fdb
robmars123 <robmars2008@gmail.com>
2017-04-24 18:26:41 -0500

    Test Commit

---
+++ Project_Managment/Project_Managment/Models/ManagerList.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Project_Managment.Models
+{
+    [Table("ManagerList")]
+    public class ManagerList
+    {
+ 
+        public int ManagerListID { get; set; }
+        public int EmployeeID { get; set; }
+        public int GroupID { get; set; }
+        public int AssetID { get; set; }
+
+        public virtual Employee Employees { get; set; }
+        public virtual Group Groups { get; set; }
+        public virtual Asset Assets { get; set; }
+        public virtual ICollection<Employee_Asset> Employees_Assets { get; set; }
+    }
+}
